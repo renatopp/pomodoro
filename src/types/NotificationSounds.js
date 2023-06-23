30be98a1a06a1927f2615b40dc340065d7d4f97d
Renato Pereira <renato.ppontes@gmail.com>
2023-06-23 14:03:54 -0300

    Fixing Notifications path

--- src/types/NotificationSounds.js
+++ src/types/NotificationSounds.js
@@ -6,18 +6,18 @@ export default {
   },
   bell: {
     name: 'Bell',
-    path: `/assets/sfxs/bell.mp3`
+    path: `/apps/pomodoro/assets/sfxs/bell.mp3`
   },
   acoustic_guitar: {
     name: 'Acoustic Guitar',
-    path: `/assets/sfxs/acoustic-guitar.mp3`
+    path: `/apps/pomodoro/assets/sfxs/acoustic-guitar.mp3`
   },
   clear_guitar: {
     name: 'Clear Guitar',
-    path: `/assets/sfxs/clear-guitar.mp3`
+    path: `/apps/pomodoro/assets/sfxs/clear-guitar.mp3`
   },
   harmonica: {
     name: 'Harmonica',
-    path: `/assets/sfxs/harmonica.wav`
+    path: `/apps/pomodoro/assets/sfxs/harmonica.wav`
   },
 }
